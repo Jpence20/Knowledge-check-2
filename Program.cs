a7c0eb7ecbe0783e2cff225f96d729d07388bd34
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-10-29 16:09:23 -0400

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+ static void Main(string[] args)
+{
+
+
+    Console.WriteLine("How many records do you want to add? ");
+    var numberOfRecords = int.Parse(Console.ReadLine());
+
+    var recordList = new List<VideoGames>();
+    for (int i = 0; i < numberOfRecords; i++)
+    {
+        // In this loop, populate the object's properties using Console.ReadLine()
+        var videogames = new VideoGames();
+
+        Console.WriteLine("Enter the value for ");
+        string name = Console.ReadLine();
+
+
+        recordList.Add(videogames);
+    }
+}
---
+++ VideoGames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class VideoGames
+{
+	public string name;
+	public string genre;
+	public string publisher;
+	public bool consoleExclusive;
+
+	public VideoGames(string gameName, string gameGenre,string gamePublisher, bool gameConsoleExculsive)
+	{
+		gameName = this.name;
+		gameGenre = this.genre;
+		gamePublisher = this.publisher;
+		gameConsoleExculsive = this.consoleExclusive;
+	}
+
+
+}
